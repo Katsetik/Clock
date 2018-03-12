b1d22b6d2022dd3747f5dff62065e1d3c36edf2a
Katsetik <Kacetovith@gmail.com>
2018-03-12 20:56:20 +0200

    Add files via upload

---
+++ ClockProject/ClockForm/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using ClockFunctional;
+using ClockForm;
+
+namespace ClockForm
+{
+    public partial class Form1 : Form
+    {
+        Thread clockThread;
+            
+        public Form1()
+        {
+            InitializeComponent();
+            clockThread = new Thread(StartClock);
+            clockThread.Start();
+        }
+
+
+        private void StartClock()
+        {
+            var clock = new Clock();
+            clock.Ticked += SetClockLabel;
+            clock.Tick();
+        }
+
+        private void SetClockLabel(object sender, TimeEventArgs e)
+        {
+            var clock = sender as Clock; 
+            lblClock.Text = String.Format("{0}{1}{2}", e.Hour, e.Minute, e.Second);
+        }
+        
+        private void RunClock()
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            clockThread.Abort();
+        }
+    }
+}
